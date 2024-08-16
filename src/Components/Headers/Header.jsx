c7bd5c0d8e0b031bc93397c1eac25dd25fb21592
roniislam999 <mdronii961@gmail.com>
2024-08-16 12:14:21 +0600

    header change to sticky

--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -1,7 +1,8 @@
 import Profile from '../../assets/profile.png'
 const Header = () => {
   return (
-    <header className=' bg-gradient-to-r from-cyan-300 to-gray-300'>
+    <header className=' bg-gradient-to-r from-cyan-300 to-gray-300 
+    sticky top-0 z-50'>
       <div className=' flex max-w-7xl mx-auto border-b-2 h-14 justify-between items-center px-4 py-10 md:px-4 lg:px-0'>
         <h1 className='text-4xl font-bold'>knowledge Cafe</h1>
         <img className='w-14 h-14 items-center' src={Profile} alt="" />
