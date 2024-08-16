acc5a144e6a557f19c50a09465b15a5d5c9cc280
roniislam999 <mdronii961@gmail.com>
2024-08-16 11:47:47 +0600

    ubdate blog buttion color combination

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -17,16 +17,17 @@ const Blog = ({ blog, handelAddToBookMark, handelMarkAsRead }) => {
           </div>
           <div className='flex items-center gap-2'>
             <span>{reading_time} min read</span>
-            <button onClick={() => handelAddToBookMark(blog)} className='text-2xl'><CiBookmark /></button>
+            <button onClick={() => handelAddToBookMark(blog)} className='text-2xl hover:bg-gray-600 hover:py-1 duration-
+            300'><CiBookmark /></button>
           </div>
         </div>
         <h2 className='text-4xl mb-4'>{title}</h2>
-        <p className='flex gap-4'>
+        <p className='flex gap-4  duration-300'>
           {
-            hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+            hashtags.map((hash, idx) => <span className='hover:text-purple-500' key={idx}><a href="">{hash}</a></span>)
           }
         </p>
-        <button onClick={() => handelMarkAsRead(id, reading_time)} className='text-purple-700 underline mt-3 text-xl cursor-pointer'>Mark as read</button>
+        <button onClick={() => handelMarkAsRead(id, reading_time)} className={`text-purple-700 tracking-wide underline mt-3 text-xl cursor-pointer  duration-300 hover:text-white`}> Mark as read</button>
       </div>
     </div>
   );
